20780f4e02084c5fdd0d0e83ee9e505b5015f0f0
Hashmatullah Azimi <hashmat.afghan2013@gmail.com>
2023-06-05 00:45:10 -0700

    Adding class Hello World

--- src/com/hello/AnotherJavaClass.java
+++
@@ -1,8 +0,0 @@
-package com.hello;
-
-public class AnotherJavaClass {
-	public static void main(String[] args) {
-		System.out.println("My File added");
-	}
-
-}
--- src/com/hello/AnotherJavaClassOne.java
+++
@@ -1,7 +0,0 @@
-package com.hello;
-
-public class AnotherJavaClassOne {
-public static void main(String[] args) {
-	System.out.println("My file Two added");
-}
-}
